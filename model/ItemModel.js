4a6e8620be6d56cc85dab412377a881159bf38c4
Oshan <git config --global user.email>
2026-04-27 10:12:31 +0530

    create Item Model

--- model/ItemModel.js
+++ model/ItemModel.js
@@ -1,74 +1,49 @@
-import { item_db } from '../db/db.js';
+const items = [];
 
 class Item {
-    #code;
-    #name;
-    #price;
-    #qty;
-
-    constructor(code, name, price, qty) {
-        this.#code  = code;
-        this.#name  = name;
-        this.#price = price;
-        this.#qty   = qty;
+    constructor(id, name, price, qty) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
     }
 
-    get code()  { return this.#code;  }
-    get name()  { return this.#name;  }
-    get price() { return this.#price; }
-    get qty()   { return this.#qty;   }
+    getName() { return this.name; }
+    getPrice() { return this.price; }
+    getQty() { return this.qty; }
 
-    set name(name)   { this.#name  = name;  }
-    set price(price) { this.#price = price; }
-    set qty(qty)     { this.#qty   = qty;   }
+    setName(name) { this.name = name; }
+    setPrice(price) { this.price = price; }
+    setQty(qty) { this.qty = qty; }
 }
 
-const addItemData = (code, name, price, qty) => {
-    let new_item = new Item(code, name, parseFloat(price), parseInt(qty));
-    item_db.push(new_item);
-}
-
-const updateItemData = (code, name, price, qty) => {
-    let obj = item_db.find(item => item.code == code);
-    if (obj) {
-        obj.name  = name;
-        obj.price = parseFloat(price);
-        obj.qty   = parseInt(qty);
-    }
-}
-
-const deleteItemData = (code) => {
-    let index = item_db.findIndex(item => item.code == code);
-    if (index !== -1) {
-        item_db.splice(index, 1);
-    }
-}
-
-const getItemData = () => {
-    return item_db;
-}
-
-const getItemByIndex = (index) => {
-    return item_db[index];
-}
-
-const getItemByCode = (code) => {
-    return item_db.find(item => item.code == code);
-}
-
-const reduceItemStock = (code, qty) => {
-    let obj = item_db.find(item => item.code == code);
-    if (obj) {
-        obj.qty = obj.qty - qty;
-    }
-}
-
-const generateItemCode = () => {
-    return "I" + String(item_db.length + 1).padStart(3, "0");
-}
-
-export {
-    addItemData, updateItemData, deleteItemData,
-    getItemData, getItemByIndex, getItemByCode,
-    reduceItemStock, generateItemCode
+const addItem = (id, name, price, qty) => {
+    if (items.some(i => i.id === id)) return false;
+    items.push(new Item(id, name, price, qty));
+    return true;
 };
+
+const updateItem = (id, name, price, qty) => {
+    const i = items.find(i => i.id === id);
+    if (i) {
+        i.setName(name);
+        i.setPrice(price);
+        i.setQty(qty);
+    }
+};
+
+const deleteItem = (id) => {
+    const index = items.findIndex(i => i.id === id);
+    if (index !== -1) items.splice(index, 1);
+};
+
+const getItems = () => items;
+
+const searchItem = (keyword) => {
+    return items.filter(i =>
+        i.id.toLowerCase().includes(keyword.toLowerCase()) ||
+        i.name.toLowerCase().includes(keyword.toLowerCase())
+    );
+};
+
+export { addItem, updateItem, deleteItem, getItems, searchItem };
